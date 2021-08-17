edbe6a86c4f9d343f35aa3391a229802929651cf
Allan Ma <allankma@gmail.com>
2021-08-17 15:20:05 -0400

    Update Token.js

--- src/components/Token.js
+++ src/components/Token.js
@@ -1,5 +1,5 @@
 function Token({value}) {
-    return <div className={ `token ${value === 1 ? "red-fill" : (value === 2 ? "blue-fill" : "")}`} />;
+    return <div className={ `token ${value === 1 ? "player-one" : (value === 2 && "player-two")}`} />;
 }
 
 export default Token;
